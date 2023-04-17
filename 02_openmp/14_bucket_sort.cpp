3eae0f5517e06d48cc7e7b1be50500470c9fbb51
YudaiNoso <noso.y.aa@m.titech.ac.jp>
2023-04-17 17:10:15 +0900

    02_openmp assignment

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -12,18 +12,33 @@ int main() {
   }
   printf("\n");
 
+
   std::vector<int> bucket(range,0); 
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
-  std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+  std::vector<int> offset(bucket);
+  std::vector<int> tmp_offset(range, 0);
+  //for (int i=0; i<range; i++){
+  //  printf("%d ",bucket[i]);
+  //}
+  //printf("\n");
+#pragma omp prallel
+  for (int k=1; k<range; k<<=1) {
+#pragma omp for
+    for (int i=0; i<range; i++)
+      tmp_offset[i] = offset[i];
+#pragma omp for 
+    for (int i=k; i<range; i++)
+      offset[i] += tmp_offset[i-k];
+  }
+  offset.insert(offset.begin(), 0);
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
+  
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
